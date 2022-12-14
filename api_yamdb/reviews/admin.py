7291d7094589741dd840237be44bb2468f2504c0
Anastasia <syusyukaloa@mail.ru>
2022-12-14 13:33:06 +0700

    CreaModels: Category, Genre, Title. Admin zone

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Category, Title
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'category')
+    list_filter = ('category',)
+    list_editable = ('category',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name',)
+    empty_value_display = '-пусто-'
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Title, TitleAdmin)

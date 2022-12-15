92dd4d00080b5411e3683115f8b399596c8af2f1
Денис Покровский <12den00@gmail.com>
2022-12-15 15:29:20 +0700

    Created models: Review, Comment

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Genre, Category, Title
+from .models import Genre, Category, Title, Review, Comment
 
 
 class TitleAdmin(admin.ModelAdmin):
@@ -27,6 +27,22 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'score', 'title', 'author')
+    list_filter = ('pub_date',)
+    search_fields = ('text',)
+    empty_value_display = '-пусто-'
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'review', 'author')
+    list_filter = ('pub_date',)
+    search_fields = ('text',)
+    empty_value_display = '-пусто-'
+
+
 admin.site.register(Genre, GenreAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Title, TitleAdmin)
+admin.site.register(Review, ReviewAdmin)
+admin.site.register(Comment, CommentAdmin)

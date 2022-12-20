2320953f766723fe97674c832f26a2f6884d12bc
Anastasia <syusyukaloa@mail.ru>
2022-12-20 14:37:54 +0700

    Release 1.11 (requirements.txt)

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -3,7 +3,10 @@ from reviews.models import Title
 
 
 class TitleFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(field_name='name')
+    name = django_filters.CharFilter(
+        field_name='name',
+        lookup_expr='icontains'
+    )
     category = django_filters.CharFilter(field_name='category__slug',)
     genre = django_filters.CharFilter(field_name='genre__slug',)
     year = django_filters.CharFilter(field_name='year')

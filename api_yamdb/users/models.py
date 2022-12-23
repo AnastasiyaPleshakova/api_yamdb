494d4c6142ee8cb0efcfe4f23710de8a0777abc9
Anastasia <syusyukaloa@mail.ru>
2022-12-23 18:11:02 +0700

    Release 4.1

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,8 @@
+import re
+
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-import re
 from rest_framework import serializers
 
 
